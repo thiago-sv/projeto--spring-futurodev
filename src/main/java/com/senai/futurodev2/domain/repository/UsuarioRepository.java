bd06a0b794330fb49fef85b49f6061c456c61a0d
thiago-sv <floriponario@gmail.com>
2022-10-10 18:12:01 -0300

    Commit de atualização

--- src/main/java/com/senai/futurodev2/domain/repository/UsuarioRepository.java
+++ src/main/java/com/senai/futurodev2/domain/repository/UsuarioRepository.java
@@ -11,23 +11,23 @@ public class UsuarioRepository {
 
     public void salvarCliente(ClienteModel clienteModel) {
         var insertTable = "insert into cliente (nome, cpf) values ()";
-        System.out.println("Cliente inserido com sucesso!" + clienteModel.toString());
+        System.out.println("Cliente inserido com sucesso!" + clienteModel);
     }
 
     public void salvarEndereco(EnderecoModel enderecoModel) {
         var insertTable = "insert into endereco () values ()";
-        System.out.println("Endeço inserido com sucesso!");
+        System.out.println("Endereço inserido com sucesso!");
     }
 
     public UsuarioDto getUsuario(String cpf) {
         var selectClienteComEndereco = "select * from cliente inner join endereco ... where cpf = :cpf";
         UsuarioDto usuarioDtoRetornadoDoSql = new UsuarioDto();
-        usuarioDtoRetornadoDoSql.setNome("Eduardo Muller");
-        usuarioDtoRetornadoDoSql.setCpf("456546464646");
-        usuarioDtoRetornadoDoSql.setCep("123456789");
-        usuarioDtoRetornadoDoSql.setCidade("São Miguel do Oeste");
+        usuarioDtoRetornadoDoSql.setNome("Thiago vieira");
+        usuarioDtoRetornadoDoSql.setCpf("12345678900");
+        usuarioDtoRetornadoDoSql.setCep("8899900");
+        usuarioDtoRetornadoDoSql.setCidade("São José");
         usuarioDtoRetornadoDoSql.setEstado("Santa Catarina");
-        usuarioDtoRetornadoDoSql.setRua("Rua Almirante Tamandaré");
+        usuarioDtoRetornadoDoSql.setRua("Rua 123");
         return usuarioDtoRetornadoDoSql;
     }
 }
